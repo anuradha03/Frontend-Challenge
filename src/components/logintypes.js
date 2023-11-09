1dab1bc3596a5e765614d4e2751129d5e0cb2693
anuradha03 <30650022+anuradha03@users.noreply.github.com>
2023-11-08 18:17:36 -0800

    created login types body

---
+++ src/components/logintypes.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { FaApple, FaFacebookF } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
+
+export default function Logintypes(){
+    const buttonStyle = {
+        width: '90%', // Adjust the width as needed
+        maxWidth: '300px', // Add max-width if necessary
+      };
+    return (
+        <div className="text-center">
+        <h1 className="text-lg font-Roboto font-bold tracking-tight text-neutral-100 sm:text-5xl">
+            Log In
+        </h1>
+        <p className="mt-10 text-lg leading-8 text-neutral-300">
+            By continuing, you agree to our 
+            <a href="#" className="text-yellow-500 cursor-pointer"> Terms of Service</a>&nbsp;
+            and 
+            <a href="#" className="text-yellow-500 cursor-pointer"> Privacy Policy</a>.
+        </p>
+
+        {/* SSO Login Buttons */}
+      <div className="mt-5 flex flex-col gap-5 items-center">
+        {/* Apple Button */}
+        <button style={buttonStyle} className="border border-white bg-transparent text-white rounded-md p-2 h-11 flex items-center justify-center">
+          <FaApple className="w-6 h-6 mr-2" />
+          Continue with Apple
+        </button>
+
+        {/* Google Button */}
+        <button style={buttonStyle} className="border border-white bg-transparent text-white rounded-md p-2 h-11 flex items-center justify-center">
+          <FcGoogle className="w-6 h-6 mr-2" />
+          Continue with Google
+        </button>
+
+            {/* Facebook Button */}
+            <button style={buttonStyle} className="border border-white bg-transparent text-white rounded-md p-2 h-11 flex items-center justify-center">
+                <span className="bg-blue-600 p-0.5 rounded-full mr-2">
+                    <FaFacebookF className="w-5 h-5 text-white" />
+                </span>
+                Continue with Facebook
+            </button>
+
+            {/* Email Button */}
+            <button style={buttonStyle} className="border border-white bg-transparent text-white rounded-md p-2 h-11 flex items-center justify-center">
+                Continue with Email
+            </button>
+        </div>
+
+        {/* Sign Up Link */}
+        <p className="mt-12 text-lg leading-8 text-neutral-300">
+            Don't have an account? 
+            <a href="#" className="text-yellow-500 cursor-pointer"> Sign Up</a>.
+        </p>
+        <div className="mt-5 flex flex-col items-center">
+            {/* Cancel Button */}
+            <button style={buttonStyle} className="mt-10 border border-white bg-transparent text-white rounded-md p-2 h-11 flex items-center justify-center">
+                Cancel
+            </button>
+        </div>
+    </div>
+    )
+}
